877279423f64a6d25cdf610760d3238e3179fa79
Mpstme Student <mpstme.student@SVKMGRP.COM>
2024-08-07 13:22:43 +0530

    updated dsa prac2

--- dsa/pra2/stacks.java
+++ dsa/pra2/stacks.java
@@ -9,7 +9,7 @@ class stacks {
         this.top = -1;
     }
 
-    public void push(int element) {
+    void push(int element) {
         if (top == capacity - 1) {
             System.out.println("Stack is full. Cannot push element.");
         } else {
@@ -18,7 +18,7 @@ class stacks {
         }
     }
 
-    public int pop() {
+    int pop() {
         if (top == -1) {
             System.out.println("Stack is empty. Cannot pop element.");
             return -1;
@@ -27,7 +27,7 @@ class stacks {
         }
     }
 
-    public int top() {
+    int top() {
         if (top == -1) {
             System.out.println("Stack is empty. No top element.");
             return -1;
@@ -37,8 +37,9 @@ class stacks {
     }
 
     public static void main(String[] args) {
-        stacks stack = new stacks(5);
 
+        stacks stack = new stacks(5);
+            
         stack.push(10);
         stack.push(20);
         stack.push(30);
@@ -51,9 +52,12 @@ class stacks {
         System.out.println("Popped element: " + stack.pop());
         System.out.println("Popped element: " + stack.pop());
         System.out.println("Popped element: " + stack.pop());
+
+        System.out.println((stack.top()==-1)? "stack is empty": ("Top element is: "+stack.top()));
+
         System.out.println("Popped element: " + stack.pop());
         System.out.println("Popped element: " + stack.pop());
 
-        System.out.println("Top element after popping: " + stack.top());
+        System.out.println((stack.top()==-1)? "stack is empty": ("Top element is: "+stack.top()));
     }
 }
